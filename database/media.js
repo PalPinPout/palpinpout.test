0b1448c9714712ee6dc0becc402d84c4a4680931
PalPinPout <162868440+PalPinPout@users.noreply.github.com>
2025-11-13 15:12:47 +0700

    Add files via upload

---
+++ database/media.js
@@ -0,0 +1,13 @@
+const videos = [
+  "https://www.youtube.com/embed/44FTAS-qT8Q?si=fTUqHXIKayXZCS1w",
+  "https://www.youtube.com/embed/k7lWtYzsKIQ?si=s27fhrSMRe_4bLgR"
+];
+
+const images = [
+  "title1.jpg",
+  "title2.jpg",
+  "title3.jpg",
+  "title4.jpg",
+  "title5.jpg",
+  "title6.jpg",
+]
